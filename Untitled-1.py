badc2943100f459f642496464d17e33adffb1868
Shouvik Raj <shouviksm3332@gmail.com>
2025-03-03 02:53:33 -0600

    Taking help from Chatgpt now

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,14 +1,17 @@
 import xarray as xr
 import matplotlib.pyplot as plt
+import cartopy.crs as ccrs 
 
-text_file = open('test.txt', 'w')
 netcdf_file1 = 'ta_Amon_reanalysis_JRA-55_195801-201912.2D.cg.nc'
 netcdf_file2 = 'ua_Amon_reanalysis_JRA-55_195801-201912.2D.cg.nc'
 xrds1 = xr.open_dataset(netcdf_file1)
-#xrds2 = xr.open_dataset(netcdf_file2)
 
-plt.plot(xrds1.ta)
+date = '2019-12-16T12:00:00.000000000'
+data_date = xrds1.sel(time=date)
+
+data_date['ta'].plot()
 plt.show()
+
 
 
 
