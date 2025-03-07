4f9606b1704081cae35471a4ef5858835c5f791d
Shouvik Raj <shouviksm3332@gmail.com>
2025-03-07 04:54:07 -0600

    animation rolling

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,17 +1,36 @@
 import xarray as xr
 import matplotlib.pyplot as plt
-#import matplotlib.animation as anim
 import numpy as np
+import matplotlib.animation as animation
 
 
 netcdf_file1 = 'ta_Amon_reanalysis_JRA-55_195801-201912.2D.cg.nc'
-netcdf_file2 = 'ua_Amon_reanalysis_JRA-55_195801-201912.2D.cg.nc'
 xrds1 = xr.open_dataset(netcdf_file1)
-print(xrds1.ta)
-date = '1958-01-16T12:00:00.000000000'
-data_date = xrds1.sel(time='2019-12-16T12:00:00.000000000')
-
-data_date['ta'].plot()
-#plt.show()
 
 
+time_steps = xrds1.time.sel(time=slice('1958-01-16T12:00:00.000000000', '2019-12-16T12:00:00.000000000'))
+
+
+fig, ax = plt.subplots()
+cbar = None  
+
+
+def update(frame):
+    global cbar
+    ax.clear()
+    
+    data_date = xrds1.sel(time=time_steps[frame])  
+    im = data_date['ta'].plot(ax=ax, add_colorbar=False)  
+    
+    
+    if cbar is None:
+        cbar = fig.colorbar(im, ax=ax)
+    
+    ax.set_title(f"Temperature at time {str(time_steps[frame].values)[:16]}")
+
+
+ani = animation.FuncAnimation(fig, update, frames=len(time_steps), interval=200)
+
+
+ani.save('temperature_animation.mp4', writer='ffmpeg')  
+plt.show()  
